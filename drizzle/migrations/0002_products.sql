62fe72a5492f3df05a95615e2191c4eed402816a
Gleydson Santos <gassantos.dev@gmail.com>
2025-04-13 13:47:39 -0300

    remove example drizzle schemas and create der

--- drizzle/migrations/0002_products.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "products" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text,
-	"email" text,
-	"created_at" timestamp DEFAULT now()
-);
--- drizzle/schema.drizzle.ts
+++ drizzle/schema.drizzle.ts
@@ -1,22 +1,8 @@
 import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: text('name'),
-  email: text('email'),
-  createdAt: timestamp('created_at').defaultNow(),
-});
-
-export const persons = pgTable('persons', {
-  id: serial('id').primaryKey(),
-  name: text('name'),
-  email: text('email'),
-  createdAt: timestamp('created_at').defaultNow(),
-});
-
-export const products = pgTable('products', {
-  id: serial('id').primaryKey(),
-  name: text('name'),
-  email: text('email'),
-  createdAt: timestamp('created_at').defaultNow(),
-});
+// export const users = pgTable('users', {
+//   id: serial('id').primaryKey(),
+//   name: text('name'),
+//   email: text('email'),
+//   createdAt: timestamp('created_at').defaultNow(),
+// });
