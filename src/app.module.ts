7c35280b8b098dce12e30c1eabb08ae37eb184cd
Gleydson Santos <gassantos.dev@gmail.com>
2025-04-13 20:04:55 -0300

    auth with rsa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './infra/auth/auth.module';
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './infra/auth/guards/auth.guard';
 
 const env = dotenv.config();
 dotenvExpand.expand(env);
@@ -17,8 +20,14 @@ export const prefix = 'ms-expenses-control/api/v1';
       load: [() => env.parsed],
     }),
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
